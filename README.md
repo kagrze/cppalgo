f734c4dbc4ffc4bb26bac9d74003184d0c715411
Karol Grzegorczyk <kgr@agh.edu.pl>
2015-05-29 18:11:31 +0200

    k-NN

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ cppalgo
 
 C++ implementations of miscellaneous algorithms
 
-- Basic algorithms
-    - [Factorial (recursive)](gcd_factorial.cpp)
-    - [Greatest common divisor (GCD) with the Euclidean algorithm](gcd_factorial.cpp)
-    - [Power (recursive)](power_sqrt.cpp)
-    - [Square root with the Babylonian method (a.k.a. the Heron's method)](power_sqrt.cpp)
-    - [Fibonacci number](hanoi_fibonacci.cpp)
-    - [Tower of Hanoi](hanoi_fibonacci.cpp)
-
+- Basic algorithms:
+ - [Factorial (recursive)](gcd_factorial.cpp)
+ - [Greatest common divisor (GCD) with the Euclidean algorithm](gcd_factorial.cpp)
+ - [Power (recursive)](power_sqrt.cpp)
+ - [Square root with the Babylonian method (a.k.a. the Heron's method)](power_sqrt.cpp)
+ - [Fibonacci number](hanoi_fibonacci.cpp)
+ - [Tower of Hanoi](hanoi_fibonacci.cpp)
 - [The knapsack problem](knapsack.cpp)
 - [N queens problem (backtracking approach)](queens.cpp)
 - [Spell checker based on the Noisy Channel Model and the Levenshtein distance](spell_checker.cpp)
--- knn.cpp
+++ knn.cpp
@@ -6,6 +6,15 @@
 #include <cassert>
 #include <cmath>
 
+/*
+ * This implementation gives the same results as the one in R
+ * You can check it by:
+ * 
+ * library(FNN)
+ * library(MASS)
+ * knn.reg(train=Boston[1:505,1:13], y=Boston[1:505,14], test=Boston[506,1:13], k=5)
+ */
+
 typedef std::vector<std::vector<float> > FloatMatrix;
 
 /*
@@ -31,25 +40,20 @@ FloatMatrix getBoston() {
     }
   }
 
+  std::random_shuffle(houses.begin(), houses.end());
   return houses;
 }
 
 /*
- * Compute euclidean distance between vectors vA and vB
+ * Compute euclidean distance between two vectors created from vectors vA and vB by taking all but the last of their elements.
  */
 float euclideanDistance(std::vector<float> vA, std::vector<float> vB) {
   assert(vA.size() == vB.size());
-  // std::cout << "vA:" << vA.at(0) << std::endl;
-  // std::cout << "vB:" << vB.at(0) << std::endl;
   auto squaredDiff = [](float a, float b) {
-    auto tmp = std::pow(a - b, 2);
-    // std::cout << "squaredDiff:" << a << " " << b << " " << tmp << std::endl;
-    return tmp;
+    return std::pow(a - b, 2);
   };
   auto sumOfSquaredDiffs = std::inner_product(vA.begin(), vA.end() - 1, vB.begin(), 0, std::plus<float>(), squaredDiff);
-  auto tmp = std::sqrt(sumOfSquaredDiffs); 
-  // std::cout << "euclideanDistance:" << tmp << std::endl;
-  return tmp;
+  return std::sqrt(sumOfSquaredDiffs);
 }
 
 float knnRegression(FloatMatrix trainSet, std::vector<float> testExample, int k) {
@@ -57,52 +61,41 @@ float knnRegression(FloatMatrix trainSet, std::vector<float> testExample, int k)
   auto transformFunction = [testExample](std::vector<float> trainExample) {
     return std::make_tuple(euclideanDistance(trainExample, testExample), trainExample.back());
   };
-  std::cout << "Before transform" << std::endl;
   std::transform(trainSet.begin(), trainSet.end(), distance_and_output.begin(), transformFunction);
-  std::cout << "Before sort. First element: " << std::get<0>(distance_and_output.front()) << " " << std::get<1>(distance_and_output.front()) << std::endl;
   std::sort(distance_and_output.begin(), distance_and_output.end(), [](std::tuple<float, float> do1, std::tuple<float, float> do2) {
     return std::get<0>(do1) < std::get<0>(do2);
   });
-  std::cout << "After sort. First element: " << std::get<0>(distance_and_output.front()) << " " << std::get<1>(distance_and_output.front()) << std::endl;
-  std::cout << "After sort. Second element: " << std::get<0>(distance_and_output[1]) << " " << std::get<1>(distance_and_output[1]) << std::endl;
-  std::cout << "After sort. Third element: " << std::get<0>(distance_and_output[2]) << " " << std::get<1>(distance_and_output[2]) << std::endl;
 
   std::vector<float> outputs(k);
   auto transformBackFunction = [](std::tuple<float, float> dist_and_out) {
     return std::get<1>(dist_and_out);
   };
   std::transform(distance_and_output.begin(), distance_and_output.begin() + k, outputs.begin(), transformBackFunction);
-  for (auto output : outputs)
-    std::cout << "Element: " << output << std::endl;
   float sum = std::accumulate(outputs.begin(), outputs.end(), 0.0);
-  std::cout << "Sum: " << sum << std::endl;
-	return sum/k;
+  return sum/k;
 }
 
 int main() {
   FloatMatrix houses = getBoston();
 
-  std::cout << houses.size() << std::endl;
-
-  std::cout << houses[0][0] << std::endl;
-  std::cout << houses[0][1] << std::endl;
-  std::cout << houses[1][0] << std::endl;
-
-  // FloatMatrix trainset = FloatMatrix(houses.begin() + 1, houses.begin() + 4);
-  // FloatMatrix testset =  FloatMatrix(houses.begin(), houses.begin() + 1);
-
-  FloatMatrix trainset = FloatMatrix(houses.begin(),    houses.end() - 50);
-  FloatMatrix testset =  FloatMatrix(houses.end() - 50, houses.end());
-
-  std::cout << trainset.size() << std::endl;
-  std::cout << testset.size()  << std::endl;
-
+  auto testsetSize = 50;
   auto k = 5;
-  auto error = 0.0;
+
+  FloatMatrix trainset = FloatMatrix(houses.begin(), houses.end() - testsetSize);
+  FloatMatrix testset =  FloatMatrix(houses.end() - testsetSize, houses.end());
+  
+  auto sum_error = 0.0;
   for (auto testExample : testset) {
     auto predicted = knnRegression(trainset, testExample, k);
     auto actual = testExample.back();
-    error += std::abs(predicted - actual) / actual;
+    auto error = std::abs(predicted - actual) / actual * 100;
+    std::printf("Actual: %5.2f, predicted: %5.2f, error: %.0f%%\n", actual, predicted, error);
+    sum_error += error;
   }
-  std::cout << "Average regression error: "<< error/testset.size() * 100 << " %" <<std::endl;
+  auto avg_error = sum_error / testset.size();
+  std::cout << "Average regression error: "<<  avg_error << "%" <<std::endl;
+
+  assert(avg_error < 25.0);
+
+  std::cout << "All tests have passed" << std::endl;
 }
